ccb6adfd12a5dcaef5d90395ffb32ae5eb9abaf9
Martin Staudt <ms@aptex.de>
2020-10-11 15:12:42 +0200

    created backend

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://mern-auth-user:YkowvdIj4qHTPQuTvJD5HmVXIdhMHCTb@cluster0.wced1.mongodb.net/mern-auth?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+};
